d1e048714f827279ae06fd04e8bf691be6a5a242
Eric Slutz <eric.slutz@icloud.com>
2024-12-14 09:35:20 -0500

    feat: create component to display books and manage updates

---
+++ myreads-app/src/components/BookShelf.js
@@ -0,0 +1,37 @@
+import PropTypes from "prop-types";
+import Book from "./Book";
+
+/**
+ * Component representing a bookshelf.
+ *
+ * @param {Object} props - The component props.
+ * @param {string} props.title - The title of the bookshelf.
+ * @param {Array} props.books - The array of books to display on the bookshelf.
+ * @param {Function} props.updateBookShelf - The function to update the bookshelf.
+ *
+ * @returns {JSX.Element} The rendered bookshelf component.
+ */
+const BookShelf = ({ title, books, updateBookShelf }) => {
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {books.map((book) => (
+            <li key={book.id}>
+              <Book book={book} updateBookShelf={updateBookShelf} />
+            </li>
+          ))}
+        </ol>
+      </div>
+    </div>
+  );
+};
+
+BookShelf.propTypes = {
+  books: PropTypes.array.isRequired,
+  title: PropTypes.string.isRequired,
+  updateBookShelf: PropTypes.func.isRequired,
+};
+
+export default BookShelf;

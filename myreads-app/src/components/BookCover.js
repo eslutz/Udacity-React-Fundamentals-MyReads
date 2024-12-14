e2ffc08622b3bb4a0718954f5a54a150d2e07615
Eric Slutz <eric.slutz@icloud.com>
2024-12-14 09:34:28 -0500

    feat: create component for a book cover

---
+++ myreads-app/src/components/BookCover.js
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+
+/**
+ * Component to display the cover of a book.
+ *
+ * @param {Object} props - The properties object.
+ * @param {Object} props.imageLinks - The image links object containing URLs for the book's images.
+ * @param {string} [props.imageLinks.thumbnail] - The URL for the book's thumbnail image.
+ *
+ * @returns {JSX.Element} A div element displaying the book cover.
+ */
+const BookCover = ({ imageLinks }) => {
+  // Get the thumbnail URL from the imageLinks object.
+  const thumbnail = imageLinks?.thumbnail ?? "";
+
+  // Style object for the book cover.
+  const coverStyle = {
+    width: 128,
+    height: 193,
+    backgroundImage: `url("${thumbnail}")`,
+  };
+
+  return <div className="book-cover" style={coverStyle}></div>;
+};
+
+BookCover.propTypes = {
+  imageLinks: PropTypes.shape({
+    thumbnail: PropTypes.string,
+  }),
+};
+
+export default BookCover;
